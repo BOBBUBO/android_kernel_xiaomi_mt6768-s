9133b1d41d541dd085a184adba399b218b08a72c
Ma Ke <make24@iscas.ac.cn>
2024-07-24 16:40:47 +0800

    spi: ppc4xx: handle irq_of_parse_and_map() errors
    [ Upstream commit 0f245463b01ea254ae90e1d0389e90b0e7d8dc75 ]
    
    Zero and negative number is not a valid IRQ for in-kernel code and the
    irq_of_parse_and_map() function returns zero on error.  So this check for
    valid IRQs should only accept values > 0.
    
    Fixes: 44dab88e7cc9 ("spi: add spi_ppc4xx driver")
    Signed-off-by: Ma Ke <make24@iscas.ac.cn>
    Link: https://patch.msgid.link/20240724084047.1506084-1-make24@iscas.ac.cn
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit f2a73a1f728e6fe765fc07c043a3d1670d854518)
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- drivers/spi/spi-ppc4xx.c
+++ drivers/spi/spi-ppc4xx.c
@@ -493,10 +493,9 @@ static int spi_ppc4xx_of_probe(struct platform_device *op)
 	}
 
 	/* Request IRQ */
-	ret = platform_get_irq(op, 0);
-	if (ret < 0)
+	hw->irqnum = irq_of_parse_and_map(np, 0);
+	if (hw->irqnum <= 0)
 		goto free_host;
-	hw->irqnum = ret;
 
 	ret = request_irq(hw->irqnum, spi_ppc4xx_int,
 			  0, "spi_ppc4xx_of", (void *)hw);
